ab8c0b1c013bf35b893e139d459bc7bfba30a54e
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-01-31 14:19:30 -0800

    Update stepper_bounce.ino
    opps

--- stepper_bounce.ino
+++ stepper_bounce.ino
@@ -1,8 +1,8 @@
 // simple bounce code to test basic operation of stepper
 #include <AccelStepper.h>
 
-#define PUL = 9;    // PUL- pin
-#define DIR = 8;    // DIR- pin
+#define PUL 9    // PUL- pin
+#define DIR 8    // DIR- pin
 
 // Define a stepper and the pins it will use
 AccelStepper stepper(1, PUL, DIR);
