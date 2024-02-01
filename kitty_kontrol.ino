d4c341ddcefae11883443e13ce02ba3f7df92033
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-01-31 18:56:56 -0800

    Create kitty_kontrol.ino
    it's a start ...

---
+++ kitty_kontrol.ino
@@ -0,0 +1,27 @@
+// kitty_kontrol.ino - code to kontrol operation of stepper motor in the cats head
+// 1/31/2024 - Idea Fab Labs, Chico
+
+#include <AccelStepper.h>
+
+#define PUL 9  // Stepper Pulse pin
+#define DIR 8  // Stepper Direction pin
+#define JUP 6  // Joystick Up pin
+#define JDN 4  // Joystick Down pin
+#define JLT 5  // Joystick Left pin
+#define JRT 7  // Joystick Right pin
+#define LLS A0  // Left Limit Switch pin
+#define RLS A1  // Right Limit Switch pin
+
+// Define the stepper and the pins used
+AccelStepper stepper(1, PUL, DIR);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Startup ...");
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(20);
+}
+
+void loop() {
+  
+}
