c3168595898049bb2221053c91f78738b71f212d
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-02-04 10:25:11 -0800

    Update kitty_kontrol.ino
    added automatic bounce mode

--- kitty_kontrol.ino
+++ kitty_kontrol.ino
@@ -1,5 +1,6 @@
 // kitty_kontrol.ino - code to kontrol operation of stepper motor in the cats head
 // 1/31/2024 - Idea Fab Labs, Chico
+// current issue: runSpeed() doesn't support acceleration ...
 
 #include <AccelStepper.h>
 
@@ -12,19 +13,25 @@
 #define JRT   7   // Joystick Right pin
 #define LLS   3   // Left Limit Switch pin
 #define RLS   2   // Right Limit Switch pin
-#define SPD   A0  // potentiometer for speed pin
+#define SPD   A0  // Potentiometer for speed pin
+//#define ACC   A1  // Potentiometer for acceleration pin
 
 // direction
 #define STOP  0
 #define CW    1
 #define CCW   2
 
-// Variables
-int MaxSpeed        = 1000; // maximum speed for stepper
-int MinSpeed        = 20;   // minimum speed for stepper
-int RUNNING         = 0;    // are we moving? and in what direction? 0=stopped 1=forward 2=reverse
-//int Acceleration  = 20;
-int Speed           = STOP; // Initial speed stopped
+// Stepper variables
+int MaxSpeed          = 500; // maximum speed for stepper
+int MinSpeed          = 25;   // minimum speed for stepper
+int Speed             = STOP; // initial speed stopped
+//int MaxAcceleration = 200;  // maximum acceleration for stepper
+//int MinAcceleration = 20;   // minimum acceleration for stepper
+//int Acceleration    = 20;   // initial acceleration rate
+int RUNNING           = STOP; // are we moving? and in what direction?
+bool AUTOMATIC        = false;// are we in automatic (bounce) mode
+
+// Joystick variables
 int JoyLeft_curr    = HIGH;
 int JoyLeft_last    = HIGH;
 int JoyRight_curr   = HIGH;
@@ -33,6 +40,8 @@ int JoyUp_curr      = HIGH;
 int JoyUp_last      = HIGH;
 int JoyDown_curr    = HIGH;
 int JoyDown_last    = HIGH;
+
+// Interrupts variables
 int stateRight_curr = HIGH;
 int stateLeft_curr  = HIGH;
 int stateRight_last = HIGH;
@@ -77,12 +86,12 @@ void loop() {
 void moveStepper() {
   if(RUNNING > STOP) {
     readSpeed();
-    if(RUNNING == CW) {
-      stepper.setSpeed(Speed);
-    } else {
+    //readAcceleration();
+    //stepper.setAcceleration(Acceleration);
+    if(RUNNING == CCW) {
       Speed = -Speed; // CCW
-      stepper.setSpeed(Speed);
     }
+    stepper.setSpeed(Speed);
   }
   stepper.runSpeed();
 }
@@ -91,23 +100,33 @@ void moveStepper() {
 void checkLimits() {
   if(stateRight_curr != stateRight_last) {
     Serial.println("Right Limit Hit! ...");
-    Speed = STOP;
     RUNNING = STOP;
-    //stepper.setAcceleration(0); // hard stop!
-    stepper.setSpeed(Speed);
+    //stepper.setAcceleration(STOP); // hard stop!
+    stepper.setSpeed(STOP);
     stepper.runSpeed(); // stop asap
     digitalWrite(LED_BUILTIN, LOW);    
     stateRight_last = stateRight_curr;
+    if(AUTOMATIC) {
+      stateRight_last = HIGH;
+      stateRight_curr = HIGH;      
+      RUNNING = CW;
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
   } else {
     if(stateLeft_curr != stateLeft_last) {
       Serial.println("Left Limit Hit! ...");
-      Speed = STOP;
       RUNNING = STOP;
-      //stepper.setAcceleration(0); // hard stop!
-      stepper.setSpeed(Speed);
+      //stepper.setAcceleration(STOP); // hard stop!
+      stepper.setSpeed(STOP);
       stepper.runSpeed(); // stop asap
       digitalWrite(LED_BUILTIN, LOW);    
       stateLeft_last = stateLeft_curr;
+      if(AUTOMATIC) {
+        stateLeft_last = HIGH;
+        stateLeft_curr = HIGH;        
+        RUNNING = CCW;
+        digitalWrite(LED_BUILTIN, HIGH);
+      }
     }
   }
 }
@@ -119,17 +138,24 @@ void readJoystick() {
   JoyUp_curr    = digitalRead(JUP);
   JoyDown_curr  = digitalRead(JDN);
 
+// Joystick left switch
   if(JoyLeft_curr != JoyLeft_last) {
     if(JoyLeft_curr == HIGH) {
       Serial.println("Stop ...");
-      Speed = STOP;
       RUNNING = STOP;
       //stepper.setAcceleration(Acceleration);
-      stepper.setSpeed(Speed);
+      stepper.setSpeed(STOP);
       digitalWrite(LED_BUILTIN, LOW);
     } else {
+      if(AUTOMATIC) {
+        AUTOMATIC = false;
+        RUNNING = STOP;
+        stepper.setSpeed(STOP);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Automatic stopped ...");
+      }  
       if(stateLeft_last == LOW) { // left limit already reached!
-        Serial.println("Limit Hit!");
+        Serial.println("Left limit active!");
         JoyLeft_last = JoyLeft_curr;
         return;
       }
@@ -144,17 +170,24 @@ void readJoystick() {
     JoyLeft_last = JoyLeft_curr;
   } 
 
+// Joystick right switch
   if(JoyRight_curr != JoyRight_last) {
     if(JoyRight_curr == HIGH) {
       Serial.println("Stop ...");
-      Speed = STOP;
       RUNNING = STOP;
       //stepper.setAcceleration(Acceleration);
-      stepper.setSpeed(Speed);
+      stepper.setSpeed(STOP);
       digitalWrite(LED_BUILTIN, LOW);
     } else {
+      if(AUTOMATIC) {
+        AUTOMATIC = false;
+        RUNNING = STOP;
+        stepper.setSpeed(STOP);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Automatic stopped ...");
+      }      
       if(stateRight_last == LOW) { // Right limit already reached!
-        Serial.println("Limit Hit!");
+        Serial.println("Right limit active!");
         JoyRight_last = JoyRight_curr;
         return;
       }
@@ -168,12 +201,48 @@ void readJoystick() {
     }
     JoyRight_last = JoyRight_curr;
   }
+
+// Joystick up switch  
+  if(JoyUp_curr != JoyUp_last) {
+    if(JoyUp_curr == LOW) {
+      if(AUTOMATIC) {
+        Serial.println("Automatic already running ...");
+        return;
+      }
+      AUTOMATIC = true;
+      RUNNING = CCW; // FIXME test for tripped limit switches and go the other way ...
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("Automatic started ...");
+    }
+    JoyUp_last = JoyUp_curr;
+  }
+  
+// Joystick down switch  
+  if(JoyDown_curr != JoyDown_last) {
+    if(JoyDown_curr == LOW) {
+      if(AUTOMATIC) {
+        AUTOMATIC = false;
+        RUNNING = STOP;
+        stepper.setSpeed(STOP);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Automatic stopped ...");
+      }
+    }
+    JoyDown_last = JoyDown_curr;
+  }  
 }
 
 // Read analog potentiometer / calculate speed
 void readSpeed() {
  Speed = map((analogRead(SPD)), 0, 1023, MinSpeed, MaxSpeed);
 }
+/*
+// Read analog potentiometer / calculate acceleration
+void readAcceleration() {
+  //Acceleration = map((analogRead(ACC)), 0, 1023, MinAcceleration, MaxAcceleration);
+  Acceleration = 200;
+}
+*/
 
 // Left limit-switch ISR
 void limitLeft() {
